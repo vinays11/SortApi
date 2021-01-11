dd68bc01cbeea2cab4ef0df16a2338828f7b6950
Vinay S <Vinay.S@Honeywell.com>
2021-01-12 01:45:06 +0530

    Updated README & code cleanup

--- SortApi/Repository/InMemoryRepository.cs
+++ SortApi/Repository/InMemoryRepository.cs
@@ -6,7 +6,6 @@
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
     public class InMemoryRepository : IRepository
